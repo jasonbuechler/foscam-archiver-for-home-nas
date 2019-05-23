17d6c6c94510ccafb3284819c5fb4a654fb93954
jasonbuechler <34217071+jasonbuechler@users.noreply.github.com>
2019-05-23 11:58:54 -0700

    Update sync-foscams-simple.sh

--- sync-foscams-simple.sh
+++ sync-foscams-simple.sh
@@ -4,12 +4,27 @@
 # (as opposed to complex Lftp scripting)
 #
 
+set fos_addr=192.168.1.101
+set http_port=:88
 set username=MYNAME
 set password=MYPASSWORD
-set foshttp=http://192.168.1.101:88
-set cgipath=/cgi-bin/CGIProxy.fcgi
-set startcmd=cmd=startFtpServer
+set cgi_path=/cgi-bin/CGIProxy.fcgi
+set start_cmd=cmd=startFtpServer
+set media_dir=/root/media/cam101
 
-#eg: http://192.168.1.101:88/cgi-bin/CGIProxy.fcgi?cmd=startFtpServer&usr=MYNAME&pwd=MYPASSWORD
+# full url to call (with curl) to start ftp server...
+# eg: http://192.168.1.101:88/cgi-bin/CGIProxy.fcgi?cmd=startFtpServer&usr=MYNAME&pwd=MYPASSWORD
 
-curl -v "$foshttp$cgipath?$startcmd&usr=$username&pwd=$password"
+curl -v "http://$fos_addr$http_port$cgi_path?$start_cmd&usr=$username&pwd=$password"
+
+lftp -u $username,$password -c "mirror -v / $media_dir" -p 50021 ftp://$fos_addr
+
+# Lftp string to locally archive
+# eg: lftp -u MYNAME,MYPASSWORD -c "[mirror command]" -p 50021 ftp://192.168.1.101
+# eg: "mirror -v / /root/media/cam101"
+#                | |
+#                | \-- local target path
+#                \---- remote site source path
+
+
+
