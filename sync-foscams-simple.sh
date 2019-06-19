fa0fba7a92becf4f61e6cf8a0e23931c6dc57b89
jasonbuechler <34217071+jasonbuechler@users.noreply.github.com>
2019-06-18 21:22:11 -0700

    Update sync-foscams-simple.sh

--- sync-foscams-simple.sh
+++ sync-foscams-simple.sh
@@ -1,4 +1,4 @@
-#!/bin/csh
+#!/bin/sh
 #
 # This single script can be used for simple mirror operations
 #  ...as opposed to complex Lftp scripting.
@@ -6,22 +6,26 @@
 #
 
 
+# this should be same for all recent h264 foscams
+cgi_cmd=/cgi-bin/CGIProxy.fcgi?cmd=startFtpServer
+
+
 # set these to reflect your camera, & archive path
-set foscam_addr=192.168.1.101
-set http_port=88
-set username=MYNAME
-set password=MYPASSWORD
-set media_dir=/root/media/cam101
+foscam_addr=192.168.1.101
+http_port=88
+username=MYNAME
+password=MYPASSWORD
+media_dir=/path/to/media/cam101
 
-# these should be same for all recent h264 foscams
-set cgi_path=/cgi-bin/CGIProxy.fcgi 
-set start_cmd=cmd=startFtpServer
+# this command pair mirrors the camera defined by the above variables
+curl -v "http://${foscam_addr}:${http_port}${cgi_cmd}&usr=${username}&pwd=${password}"
+lftp -u $username,$password -e "mirror -v / $media_dir" -p 50021 ftp://${foscam_addr}
 
-curl -v "http://${foscam_addr}:${http_port}${cgi_path}?${start_cmd}&usr=${username}&pwd=${password}"
 
-lftp -u ${username},${password} -e "mirror -v / ${media_dir}" -p 50021 ftp://${foscam_addr}
+
 
 #=================================================================================
+#
 # full url to call (with curl) to start ftp server...
 # eg: http://192.168.1.101:88/cgi-bin/CGIProxy.fcgi?cmd=startFtpServer&usr=MYNAME&pwd=MYPASSWORD
 #
@@ -33,14 +37,12 @@ lftp -u ${username},${password} -e "mirror -v / ${media_dir}" -p 50021 ftp://${f
 #               | |
 #               | \-- local system target path (e.g. writeable storage)
 #               \---- remote system source path (e.g. camera FTP root)
+#
+#
+# for additional cameras: copy/paste/modify "set these" lines above, then,
+# after those, copy/paste the "curl" and "lftp" lines.
+# Cameras will *not* mirror simultaneously unless the 'lftp' line has a
+# trailing ampersand (&) to force it into the background.
+#
 #=================================================================================
-
-##############
-##
-## for additional cameras: copy/paste/modify "set" lines above
-## and, after those, copy/paste the "curl" and "lftp" lines.
-## Cameras will *not* mirror simultaneously.
-##
-##############
-
 
